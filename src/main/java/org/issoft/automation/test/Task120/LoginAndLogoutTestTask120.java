08cff8791e32fec655e16d11b9edff7087a4e59b
olgamorozova1 <marozavavolha@gmail.com>
2020-12-14 10:31:33 +0300

    Fixes

--- src/main/java/org/issoft/automation/test/Task120/LoginAndLogoutTestTask120.java
+++ src/main/java/org/issoft/automation/test/Task120/LoginAndLogoutTestTask120.java
@@ -1,23 +1,24 @@
 package org.issoft.automation.test.Task120;
 
 import org.issoft.automation.page.Task110.MainPageTask110;
+import org.issoft.automation.page.Task120.MainPageTask120;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginAndLogoutTestTask120 extends TestBaseTask120 {
-    MainPageTask110 mainPageTask110 = new MainPageTask110(driver);
+    MainPageTask120 mainPageTask120 = new MainPageTask120(driver);
 
     @Test
     public void loginWithCorrectCredentials() {
-        String userName = mainPageTask110.login("seleniumtests@tut.by", "123456789zxcvbn").getUserNameLinkText();
+        String userName = mainPageTask120.login("seleniumtests@tut.by", "123456789zxcvbn").getUserNameLinkText();
         assertEquals("Selenium Test", userName);
     }
 
     @Test
     public void logout() {
-        mainPageTask110.login("seleniumtests@tut.by", "123456789zxcvbn").logout();
-        String loginLinkText = driver.findElement(mainPageTask110.getLoginLink()).getText();
+        mainPageTask120.login("seleniumtests@tut.by", "123456789zxcvbn").logout();
+        String loginLinkText = driver.findElement(mainPageTask120.getLoginLink()).getText();
         assertEquals("Войти", loginLinkText);
     }
 }
